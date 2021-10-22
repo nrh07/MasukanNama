a4033ada78d6d97dc7ef59fd2b9dbe374f65f985
nrh07 <92972693+nrh07@users.noreply.github.com>
2021-10-22 21:59:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/masukannama/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.masukannama;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    //Deklarasikan Variabel
+    EditText TextNama;
+    TextView Hasil;
+    @Override    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Panggil Variabel Berdasarkan id
+        TextNama=(EditText)findViewById(R.id.TxtNama);
+        Hasil=(TextView)findViewById(R.id.Label2);
+    }
+
+    //Buat Method TampilNama Sesuai Dengan property Onclick pada button
+    public void TampilNama(View v){
+        Hasil.setText("Nama Anda: "+TextNama.getText());
+    }
+}
